11d2ab9636b2542eec9fd80806ca4bbbf733fc08
NORZACO <caspermotive4@gmail.com>
2024-06-03 14:01:46 +0200

    initial commit

---
+++ src_importer.py
@@ -0,0 +1,7 @@
+# pylint: disable=unused-import
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from moviepy.editor import ImageSequenceClip, AudioFileClip, TextClip, CompositeVideoClip
+from PIL import Image
+from dotenv import load_dotenv
